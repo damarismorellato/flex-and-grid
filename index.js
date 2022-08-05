c81001bf928625da484b484b3eb2372c039f63ba
damarismorellato <dls.morellato19@gmail.com>
2022-08-05 18:39:58 -0300

    Finalizando o layout com grid

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-/* Deixando o menu lateral ativo e inativo */
+const botaoMenu = document.querySelector('.cabecalho__menu')
+const menu = document.querySelector('.menu-lateral')
 
-const botaoMenu = document.querySelector('.cabecalho__menu');
-const menuLateral = document.querySelector('.menu__lateral');
-
-botaoMenu.addEventListener( 'click', () => {
-    menuLateral.classList.toggle('menu__lateral--ativo')
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-lateral--ativo')
 })
